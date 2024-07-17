dfd17fcce90c240da749cbd01fa99b1b9c607098
nkj-14 <jadhav_2001ee21@iitp.ac.in>
2024-07-17 15:19:40 +0530

    string contact

--- JEDI2.0-FLIPFIT-H-JAVA-POS/src/com/flipkart/bean/Admin.java
+++ JEDI2.0-FLIPFIT-H-JAVA-POS/src/com/flipkart/bean/Admin.java
@@ -2,7 +2,7 @@ package com.flipkart.bean;
 
 public class Admin extends User {
 
-    public Admin(String username, String Name, String Email, int contactNo, int age, String password, String ID) {
-        super(username, Name, Email, contactNo, age, password, ID);
+    public Admin(String username, String Name, String Email, String contactNo, int age, String password, String ID) {
+        super("bean", "BeanAdmin", "bean@gmail.com", "1234567890", 0, "bean@1234", "0");
     }
 }
