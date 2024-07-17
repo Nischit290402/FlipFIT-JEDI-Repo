c1a06cc31cae5449fd83aba1fdb937d71016757e
isha141 <ishajindal999@gmail.com>
2024-07-17 16:15:38 +0530

    User Client is added

---
+++ JEDI2.0-FLIPFIT-H-JAVA-POS/src/com/flipkart/client/ClientLogin.java
@@ -0,0 +1,30 @@
+package com.flipkart.client;
+
+
+
+import com.flipkart.bean.Login;
+import java.util.Scanner;
+
+public class ClientLogin {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for email and password
+        System.out.print("Enter email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        // Create a new Login object with user input
+        Login login = new Login(email, password);
+
+        // Output the email and password using the instance 'login'
+        System.out.println("Email: " + login.getEmail());
+        System.out.println("Password: " + login.getPassword());
+
+        scanner.close();
+        
+//        handle login 
+	}
+}
--- JEDI2.0-FLIPFIT-H-JAVA-POS/src/com/flipkart/client/FlipfitApplication.java
+++ JEDI2.0-FLIPFIT-H-JAVA-POS/src/com/flipkart/client/FlipfitApplication.java
@@ -4,4 +4,5 @@ public class FlipfitApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to Flipfit");
     }
+    
 }
