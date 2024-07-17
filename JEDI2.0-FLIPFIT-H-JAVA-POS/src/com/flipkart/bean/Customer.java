1edd2cbba343e3ba6375c97d4117135d85d5d925
nkj-14 <jadhav_2001ee21@iitp.ac.in>
2024-07-17 17:01:52 +0530

    update beans

--- JEDI2.0-FLIPFIT-H-JAVA-POS/src/com/flipkart/bean/Customer.java
+++ JEDI2.0-FLIPFIT-H-JAVA-POS/src/com/flipkart/bean/Customer.java
@@ -1,5 +1,4 @@
 package com.flipkart.bean;
-
 public class Customer extends User {
 
     public Customer(String username, String Name, String Email, String contactNo, int age, String password, String ID) {
